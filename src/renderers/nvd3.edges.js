a8a550a7ad4c7c6532f6cc0418649e5cd574f1da
Richard Jones <richard@cottagelabs.com>
2018-07-26 12:49:23 +0100

    improve word wrap algorithm

--- src/renderers/nvd3.edges.js
+++ src/renderers/nvd3.edges.js
@@ -78,6 +78,189 @@ $.extend(edges, {
                 var maxHeight = params.maxHeight;
                 var lineHeight = params.lineHeight || 1.2;
                 var minimumLineLength = params.minimumLineLength || 3;
+                var wordBreaks = params.wordBreaks || [" ", "\t"];
+                var minChunkSize = params.minHyphenSize || 3;
+
+                function _isMidWord(currentLine, remainder) {
+                    var leftChar = $.inArray(currentLine[currentLine.length - 1], wordBreaks) === -1;
+                    var rightChar = $.inArray(remainder[0], wordBreaks) === -1;
+                    return leftChar && rightChar;
+                }
+
+                function _toPrevSpace(currentLine) {
+                    for (var i = currentLine.length - 1; i >= 0; i--) {
+                        var char = currentLine[i];
+                        if ($.inArray(char, wordBreaks) !== -1) {
+                            return currentLine.length - i;
+                        }
+                    }
+                    return -1;
+                }
+
+                function _toNextSpace(remainder) {
+                    for (var i = 0; i < remainder.length; i++) {
+                        var char = remainder[i];
+                        if ($.inArray(char, wordBreaks) !== -1) {
+                            return i + 1;
+                        }
+                    }
+                    return -1;
+                }
+
+                function _backtrack(count, currentLine, remainder) {
+                    for (var i = 0; i < count; i++) {
+                        remainder.unshift(currentLine.pop());
+                    }
+                }
+
+                function _isTooLong(tspan) {
+                    return tspan.node().getComputedTextLength() >= maxWidth
+                }
+
+                function separate(text) {
+                    // get the current content then clear the text element
+                    var chars = text.text().trim().split("");
+                    text.text(null);
+
+                    // set up registries for the text lines that they will create
+                    var lines = [];
+
+                    // create a tspan for working in - we need it to calculate line widths dynamically
+                    var x = text.attr("x");
+                    var tspan = text.append("tspan").attr("x", x).attr("y", 0);
+
+                    // record the current line
+                    var currentLine = [];
+
+                    // for each character in the text, push to the current line, assign to the tspan, and then
+                    // check if we have exceeded the allowed max width
+                    while (chars.length > 0) {
+                        var char = chars.shift();
+                        currentLine.push(char);
+                        tspan.text(currentLine.join(""));
+
+                        var maxed = false;
+                        var hyphenated = false;
+                        while(_isTooLong(tspan)) {
+                            // record that we pushed the tspan to the limit
+                            maxed = true;
+
+                            // if we already added a hyphen, remove it
+                            if (hyphenated) {
+                                currentLine.splice(currentLine.length - 1);
+                                hyphenated = false;
+                            }
+
+                            // if we have exceeded the max width back-track 1
+                            _backtrack(1, currentLine, chars);
+
+                            if (_isMidWord(currentLine, chars)) {
+                                var toPrevSpace = _toPrevSpace(currentLine);
+
+                                /*
+                                var toNextSpace = _toNextSpace(chars);
+                                if (toPrevSpace === -1 && toNextSpace === -1) {
+                                    _backtrack(1, currentLine, chars);
+                                    currentLine.push("-");
+                                    hyphenated = true;
+                                } else if (toPrevSpace === -1 && toNextSpace - 1 > minChunkSize) {
+                                    _backtrack(1, currentLine, chars);
+                                    currentLine.push("-");
+                                    hyphenated = true;
+                                } else if (toPrevSpace === -1 && toNextSpace - 1 < minChunkSize) {
+                                    _backtrack(minChunkSize - toNextSpace, currentLine, chars)
+
+                                } else if (toPrevSpace - 1 > minChunkSize && toNextSpace === -1) {
+                                    _backtrack(1, currentLine, chars);
+                                    currentLine.push("-");
+                                    hyphenated = true;
+                                } else if (toPrevSpace - 1 > minChunkSize && toNextSpace - 1 > minChunkSize) {
+                                    _backtrack(1, currentLine, chars);
+                                    currentLine.push("-");
+                                    hyphenated = true;
+                                }*/
+
+                                if (toPrevSpace === -1 || toPrevSpace - 1 > minChunkSize) {
+                                    _backtrack(1, currentLine, chars);
+                                    currentLine.push("-");
+                                    hyphenated = true;
+                                } else {
+                                    _backtrack(toPrevSpace, currentLine, chars);
+                                }
+                            }
+
+                            currentLine = currentLine.join("").trim().split("");
+                            tspan.text(currentLine.join(""));
+                        }
+
+                        // if we didn't yet fill the tspan, continue adding characters
+                        if (!maxed && chars.length > 0) {
+                            continue;
+                        }
+
+                        // otherwise, move on to the next line
+                        if (maxed || chars.length === 0) {
+                            lines.push(currentLine);
+                            currentLine = [];
+                        }
+                    }
+
+                    // create all the tspans
+                    tspan.remove();
+                    var tspans = [];
+                    for (var i = 0; i < lines.length; i++) {
+                        tspan = text.append("tspan").attr("x", x).attr("y", 0);
+                        tspan.text(lines[i].join(""));
+                        tspans.push(tspan);
+                    }
+
+                    return tspans;
+                }
+
+                function distribute(text, tspans) {
+                    var imax = tspans.length;
+                    var pmax = lineHeight * (imax - 1);
+                    var dy = parseFloat(text.attr("dy"));
+
+                    for (var j = 0; j < tspans.length; j++) {
+                        var pos = (lineHeight * j) - (pmax / 2.0) + dy;
+                        var tspan = tspans[j];
+                        tspan.attr("dy", pos + "em");
+                    }
+                }
+
+                function reduce(text, tspans) {
+                    var reduced = false;
+                    var box = text.node().getBBox();
+                    if (box.height > maxHeight && tspans.length > 1) {
+                        tspans[tspans.length - 1].remove();
+                        tspans.pop();
+                        var line = tspans[tspans.length - 1].text();
+                        if (line.length > 3) {
+                            line = line.substring(0, line.length - 3) + "...";
+                        }
+                        tspans[tspans.length - 1].text(line);
+                        reduced = true;
+                    }
+                    return reduced;
+                }
+
+                d3.selectAll(axisSelector + " .tick text").each(function(i, e) {
+                    var text = d3.select(this);
+                    var tspans = separate(text);
+                    do {
+                        distribute(text, tspans);
+                    }
+                    while (reduce(text, tspans))
+                });
+            },
+
+            wrapLabelsOld : function(params) {
+                var axisSelector = params.axisSelector;
+                var maxWidth = params.maxWidth;
+                var maxHeight = params.maxHeight;
+                var lineHeight = params.lineHeight || 1.2;
+                var minimumLineLength = params.minimumLineLength || 3;
 
                 // to make room for rounding errors and hyphens
                 maxWidth = maxWidth - 5;
