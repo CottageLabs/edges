72a8f6a73aedade7db1d3ddfb3f953f043a83051
Richard Jones <richard@cottagelabs.com>
2019-05-20 17:54:27 +0100

    add renderer which can present RefiningANDTermSelector as a dropdown list of values using the bootstrap3 dropdown styles

---
+++ src/renderers/bs3.DropdownFacetRenderer.js
@@ -0,0 +1,113 @@
+$.extend(true, edges, {
+    bs3 : {
+        newDropdownFacetRenderer: function (params) {
+            return edges.instantiate(edges.bs3.DropdownFacetRenderer, params, edges.newRenderer);
+        },
+        DropdownFacetRenderer: function (params) {
+
+            ///////////////////////////////////////
+            // parameters that can be passed in
+
+            // formatter for count display
+            this.countFormat = edges.getParam(params.countFormat, false);
+
+            // namespace to use in the page
+            this.namespace = "edges-bs3-dropdown-facet";
+
+            this.draw = function () {
+                // for convenient short references ...
+                var ts = this.component;
+                var namespace = this.namespace;
+
+                // sort out all the classes that we're going to be using
+                var containerClass = edges.css_classes(namespace, "container", this);
+                var resultClass = edges.css_classes(namespace, "result", this);
+                var valClass = edges.css_classes(namespace, "value", this);
+                var removeClass = edges.css_classes(namespace, "remove", this);
+
+                var buttonId = edges.css_id(namespace, "button", this);
+
+                // this is what's displayed in the body if there are no results
+                var results = '<li class="disabled">Loading...</li>';
+                if (ts.values !== false) {
+                    results = '<li class="disabled">No data available</li>';
+                }
+
+                // render a list of the values
+                if (ts.values && ts.values.length > 0) {
+                    results = '<li class="dropdown-header">' + ts.display + '</li>';
+
+                    // get the terms of the filters that have already been set
+                    var filterTerms = [];
+                    for (var i = 0; i < ts.filters.length; i++) {
+                        filterTerms.push(ts.filters[i].term.toString());
+                    }
+
+                    if (filterTerms.length > 0) {
+                        results += '<li class="' + resultClass + '"><a href="#" class="' + removeClass + '">\
+                                Clear filter</a></li>';
+                    }
+
+                    // render each value, if it is not also a filter that has been set
+                    var first = true;
+                    for (var i = 0; i < ts.values.length; i++) {
+                        var val = ts.values[i];
+                        if ($.inArray(val.term.toString(), filterTerms) === -1) {   // the toString() helps us normalise other values, such as integers
+                            var count = val.count;
+                            if (this.countFormat) {
+                                count = this.countFormat(count)
+                            }
+                            if (first && filterTerms.length > 0) {
+                                results += '<li role="separator" class="divider"></li>';
+                                first = false;
+                            }
+                            results += '<li class="' + resultClass + '"><a href="#" class="' + valClass + '" data-key="' + edges.escapeHtml(val.term) + '">' +
+                                edges.escapeHtml(val.display) + " (" + count + ")</a></li>";
+                        }
+                    }
+                }
+
+                var resultFrag = '<ul class="dropdown-menu" aria-labelledby="' + buttonId + '"> \
+                    ' + results + '\
+                </ul>';
+
+                // if we want the active filters, render them
+                var filterFrag = ts.display;
+                if (ts.filters.length > 0) {
+                    filterFrag = ts.filters.map(function(x) {return x.display}).join("; ");
+                }
+
+                var buttonFrag = '<button class="btn btn-sm btn-default dropdown-toggle" type="button" id="' + buttonId + '" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">\
+                    ' + filterFrag + ' \
+                    <span class="caret"></span> \
+                </button>';
+
+                var frag = '<div class="' + containerClass + '"><div class="dropdown">' + buttonFrag + resultFrag + '</div></div>';
+
+                // now render it into the page
+                ts.context.html(frag);
+
+                // sort out the selectors we're going to be needing
+                var valueSelector = edges.css_class_selector(namespace, "value", this);
+                var filterRemoveSelector = edges.css_class_selector(namespace, "remove", this);
+
+                // for when a value in the facet is selected
+                edges.on(valueSelector, "click", this, "termSelected");
+                // for when a filter remove button is clicked
+                edges.on(filterRemoveSelector, "click", this, "removeFilter");
+            };
+
+            /////////////////////////////////////////////////////
+            // event handlers
+
+            this.termSelected = function (element) {
+                var term = this.component.jq(element).attr("data-key");
+                this.component.selectTerm(term);
+            };
+
+            this.removeFilter = function (element) {
+                this.component.clearFilters({triggerQuery: true});
+            };
+        }
+    }
+});
