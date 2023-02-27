3288045e88a8511a904ea7748b661eea54b7ac7a
Aga Domanska <aga@cottagelabs.com>
2023-02-27 09:21:04 +0000

    bs5 renderers

---
+++ src/renderers/bs5/FacetFilterSetter.js
@@ -0,0 +1,201 @@
+// requires: $
+// requires: edges
+// requires: edges.util
+
+if (!window.hasOwnProperty("edges")) { edges = {}}
+if (!edges.hasOwnProperty("renderers")) { edges.renderers = {}}
+if (!edges.renderers.hasOwnProperty("bs5")) { edges.renderers.bs5 = {}}
+
+edges.renderers.bs5.FacetFilterSetter = class extends edges.Renderer {
+    constructor(params) {
+        super(params);
+
+        // whether the facet should be open or closed
+        // can be initialised and is then used to track internal state
+        this.open = edges.util.getParam(params, "open", false);
+
+        // whether the facet can be opened and closed
+        this.togglable = edges.util.getParam(params, "togglable", true);
+
+        // whether the count should be displayed along with the term
+        // defaults to false because count may be confusing to the user in an OR selector
+        this.showCount = edges.util.getParam(params, "showCount", false);
+
+        // The display title for the facet
+        this.facetTitle = edges.util.getParam(params, "facetTitle", "Untitled");
+
+        this.intro = edges.util.getParam(params, "intro", false);
+
+        this.openIcon = edges.util.getParam(params, "openIcon", "glyphicon glyphicon-plus");
+
+        this.closeIcon = edges.util.getParam(params, "closeIcon", "glyphicon glyphicon-minus");
+
+        this.layout = edges.util.getParam(params, "layout", "left");
+
+        // namespace to use in the page
+        this.namespace = "edges-bs5-facet-filter-setter";
+    }
+
+    draw() {
+        // for convenient short references ...
+        var comp = this.component;
+        var namespace = this.namespace;
+
+        // sort out all the classes that we're going to be using
+        var filterClass = edges.util.allClasses(namespace, "filter", this);
+        var valClass = edges.util.allClasses(namespace, "value", this);
+        var filterRemoveClass = edges.util.allClasses(namespace, "filter-remove", this);
+        var facetClass = edges.util.allClasses(namespace, "facet", this);
+        var headerClass = edges.util.allClasses(namespace, "header", this);
+        var bodyClass = edges.util.allClasses(this.namespace, "body", this);
+        var introClass = edges.util.allClasses(this.namespace, "intro", this);
+        var countClass = edges.util.allClasses(namespace, "count", this);
+
+        var toggleId = edges.util.htmlID(namespace, "toggle", this);
+        var resultsId = edges.util.htmlID(namespace, "results", this);
+
+        var filters = "";
+        for (var i = 0; i < comp.filters.length; i++) {
+            var filter = comp.filters[i];
+            var id = filter.id;
+            var display = filter.display;
+            var count = comp.filter_counts[id];
+            var active = comp.active_filters[id];
+
+            if (count === undefined) {
+                count = 0;
+            }
+
+            filters += '<div class="' + filterClass + '">';
+
+            if (active) {
+                filters += '<strong>' + display;
+                if (this.showCount) {
+                    filters += " (" + count + ")";
+                }
+                filters += '&nbsp;<a href="#" class="' + filterRemoveClass + '" data-filter="' + edges.util.escapeHtml(id) + '">';
+                filters += '<i class="material-symbols-outlined"></i></a>';
+                filters += "</strong>";
+            } else {
+                filters += '<a href="#" class="' + valClass + '" data-filter="' + edges.util.escapeHtml(id) + '">' + display + "</a>";
+                if (this.showCount) {
+                    filters += ' <span class="' + countClass + '">(' + count + ')</span>';
+                }
+            }
+
+            filters += "</div>";
+        }
+
+        var header = this.headerLayout({toggleId: toggleId});
+
+        var introFrag = "";
+        if (this.intro !== false) {
+            introFrag = '<div class="' + introClass + '">' + this.intro + '</div>';
+        }
+
+        // render the overall facet
+        var frag = '<div class="' + facetClass + '">\
+                    <div class="' + headerClass + '"><div class="row"> \
+                        <div class="col-md-12">\
+                            ' + header + '\
+                        </div>\
+                    </div></div>\
+                    <div class="' + bodyClass + '">\
+                        <div class="row" style="display:none" id="' + resultsId + '">\
+                            <div class="col-md-12">\
+                                ' + introFrag + '\
+                                {{FILTERS}}\
+                            </div>\
+                        </div></div>\
+                    </div>';
+
+        // substitute in the component parts
+        frag = frag.replace(/{{FILTERS}}/g, filters);
+
+        // now render it into the page
+        comp.context.html(frag);
+
+        // trigger all the post-render set-up functions
+        this.setUIOpen();
+
+        // sort out the selectors we're going to be needing
+        var valueSelector = edges.util.jsClassSelector(namespace, "value", this);
+        var filterRemoveSelector = edges.util.jsClassSelector(namespace, "filter-remove", this);
+        var toggleSelector = edges.util.idSelector(namespace, "toggle", this);
+
+        // for when a value in the facet is selected
+        edges.on(valueSelector, "click", this, "filterSelected");
+        // for when the open button is clicked
+        edges.on(toggleSelector, "click", this, "toggleOpen");
+        // for when a filter remove button is clicked
+        edges.on(filterRemoveSelector, "click", this, "removeFilter");
+    }
+
+    headerLayout(params) {
+        var toggleId = params.toggleId;
+        var iconClass = edges.util.allClasses(this.namespace, "icon", this);
+
+        if (this.layout === "left") {
+            var tog = this.facetTitle;
+            if (this.togglable) {
+                tog = '<a href="#" id="' + toggleId + '"><i class="' + this.openIcon + '"></i>&nbsp;' + tog + "</a>";
+            }
+            return tog;
+        } else if (this.layout === "right") {
+            var tog = "";
+            if (this.togglable) {
+                tog = '<a href="#" id="' + toggleId + '">' + this.facetTitle + '&nbsp;<i class="' + this.openIcon + ' ' + iconClass + '"></i></a>';
+            } else {
+                tog = this.facetTitle;
+            }
+            return tog;
+        }
+    }
+
+    setUIOpen() {
+        // the selectors that we're going to use
+        var resultsSelector = edges.util.idSelector(this.namespace, "results", this);
+        var toggleSelector = edges.util.idSelector(this.namespace, "toggle", this);
+
+        var results = this.component.jq(resultsSelector);
+        var toggle = this.component.jq(toggleSelector);
+
+        var openBits = this.openIcon.split(" ");
+        var closeBits = this.closeIcon.split(" ");
+
+        if (this.open) {
+            var i = toggle.find("i");
+            for (var j = 0; j < openBits.length; j++) {
+                i.removeClass(openBits[j]);
+            }
+            for (var j = 0; j < closeBits.length; j++) {
+                i.addClass(closeBits[j]);
+            }
+            results.show();
+        } else {
+            var i = toggle.find("i");
+            for (var j = 0; j < closeBits.length; j++) {
+                i.removeClass(closeBits[j]);
+            }
+            for (var j = 0; j < openBits.length; j++) {
+                i.addClass(openBits[j]);
+            }
+            results.hide();
+        }
+    }
+
+    filterSelected(element) {
+        var filter_id = this.component.jq(element).attr("data-filter");
+        this.component.addFilter(filter_id);
+    }
+
+    removeFilter(element) {
+        var filter_id = this.component.jq(element).attr("data-filter");
+        this.component.removeFilter(filter_id);
+    }
+
+    toggleOpen(element) {
+        this.open = !this.open;
+        this.setUIOpen();
+    }
+}
---
+++ src/templates/bs5/Facetview.js
@@ -0,0 +1,138 @@
+// requires: edges
+// requires: edges.util
+
+if (!window.hasOwnProperty("edges")) { edges = {}}
+if (!edges.hasOwnProperty("templates")) { edges.templates = {}}
+if (!edges.templates.hasOwnProperty("bs5")) { edges.templates.bs5 = {}}
+
+edges.templates.bs5.Facetview = class extends edges.Template{
+    constructor(params) {
+        super(params);
+
+        this.facetHeader = edges.util.getParam(params, "facetHeader", false);
+
+        this.namespace = "edges-bs5-facetview";
+        this.edge = false;
+    }
+
+    draw(edge) {
+        this.edge = edge;
+
+        let containerClass = edges.util.styleClasses(this.namespace, "container");
+        let facetsClass = edges.util.styleClasses(this.namespace, "facets");
+        let facetClass = edges.util.styleClasses(this.namespace, "facet");
+        let panelClass = edges.util.styleClasses(this.namespace, "panel");
+        let controllerClass = edges.util.styleClasses(this.namespace, "search-controller");
+        let selectedFiltersClass = edges.util.styleClasses(this.namespace, "selected-filters");
+        let pagerClass = edges.util.styleClasses(this.namespace, "pager");
+        let searchingClass = edges.util.styleClasses(this.namespace, "searching");
+        let resultsClass = edges.util.styleClasses(this.namespace, "results");
+        let facetHeaderClass = edges.util.styleClasses(this.namespace, "facet-header");
+        let facetContainerClass = edges.util.styleClasses(this.namespace, "facet-container");
+
+        // the facet view object to be appended to the page
+        let thefacetview = `<div class="${containerClass}"><div class="row">`;
+
+        // if there are facets, give them span3 to exist, otherwise, take up all the space
+        let facets = edge.category("facet");
+        let facetContainers = "";
+
+        if (facets.length > 0) {
+            let facetHeader = "";
+            if (this.facetHeader !== false) {
+                facetHeader = `<div class="${facetHeaderClass}">${this.facetHeader}</div>`
+            }
+            thefacetview += `<div class="col-md-3 ${facetContainerClass}">
+                ${facetHeader}
+                <div class="${facetsClass}">{{FACETS}}</div>
+            </div>
+            <div class="col-md-9" class="${panelClass}">`;
+
+            for (let i = 0; i < facets.length; i++) {
+                facetContainers += `<div class="${facetClass}"><div id="${facets[i].id}"></div></div>`;
+            }
+        } else {
+            thefacetview += `<div class="col-md-12" class="${panelClass}">`;
+        }
+
+        // make space for the search options container at the top
+        let controller = edge.category("controller");
+        if (controller.length > 0) {
+            thefacetview += `<div class="row">
+                <div class="col-md-12">
+                    <div class="${controllerClass}">
+                        <div id="${controller[0].id}"></div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        // make space for the selected filters
+        let selectedFilters = edge.category("selected-filters");
+        if (selectedFilters.length > 0) {
+            thefacetview += `<div class="row">
+                <div class="col-md-12">
+                    <div class="${selectedFiltersClass}">
+                        <div id="${selectedFilters[0].id}"></div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        // make space at the top for the page
+        let topPagers = edge.category("top-pager");
+        if (topPagers.length > 0) {
+            thefacetview += `<div class="row">
+                <div class="col-md-12">
+                    <div class="${pagerClass}">
+                        <div id="${topPagers[0].id}"></div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        // loading notification (note that the notification implementation is responsible for its own visibility)
+        let loading = edge.category("searching-notification");
+        if (loading.length > 0) {
+            thefacetview += `<div class="row">
+                <div class="col-md-12">
+                    <div class="${searchingClass}">
+                        <div id="${loading[0].id}"></div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        // insert the frame within which the results actually will go
+        let results = edge.category("results");
+        if (results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+                thefacetview += `<div class="row">
+                    <div class="col-md-12">
+                        <div class="${resultsClass}" dir="auto">
+                            <div id="${results[i].id}"></div>
+                        </div>
+                    </div>
+                </div>`;
+            }
+        }
+
+        // make space at the bottom for the pager
+        let bottomPagers = edge.category("bottom-pager");
+        if (bottomPagers.length > 0) {
+            thefacetview += `<div class="row">
+                <div class="col-md-12">
+                    <div class="${pagerClass}">
+                        <div id="${bottomPagers[0].id}"></div>
+                    </div>
+                </div>
+            </div>`;
+        }
+
+        // close off all the big containers and return
+        thefacetview += `</div></div></div>`;
+        thefacetview = thefacetview.replace(/{{FACETS}}/g, facetContainers);
+
+        edge.context.html(thefacetview);
+    }
+}
