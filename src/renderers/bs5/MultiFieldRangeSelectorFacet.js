f66c959716f4fe644b2a167a1ac575e85f88cd6d
Aga Domanska <aga@cottagelabs.com>
2023-03-09 13:51:34 +0000

    style range selector facet

--- src/renderers/bs5/MultiFieldRangeSelectorFacet.js
+++ src/renderers/bs5/MultiFieldRangeSelectorFacet.js
@@ -118,7 +118,7 @@ edges.renderers.bs5.MultiFieldRangeSelectorFacet = class extends edges.Renderer 
         let removeFrag = "";
         if (existingField) {
             let removeClass = edges.util.allClasses(this.namespace, "remove", this);
-            removeFrag = `<button class="${removeClass}">X</button>`;
+            removeFrag = `<button class="btn btn-info btn-secondary ${removeClass}">Remove filter X</button>`;
         }
 
         let selectorClass = edges.util.allClasses(this.namespace, "selector", this);
