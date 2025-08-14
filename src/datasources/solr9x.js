4b0df43838acdeb461166d0941f71bacd889b386
kush.varade.19 <kush.varade.19@gmail.com>
2025-08-14 10:23:03 +0530

    Handled query for first and last name

--- src/datasources/solr9x.js
+++ src/datasources/solr9x.js
@@ -1151,21 +1151,13 @@ function _es2solr({ query }) {
             if (esQueryString == "*") {
               queryPart += ` ${operator} ${field}:${esQueryString}`;
             } else {
-              if (fields.length > 1) {
-                queryPart += ` ${operator} ${field}:(${esQueryString})`;
-              } else {
-                queryPart += ` ${operator} ${field}:"${esQueryString}"`;
-              }
+              queryPart += ` ${operator} ${field}:(${esQueryString})`;
             }
           } else {
             if (esQueryString == "*") {
               queryPart = `${field}:${esQueryString}`;
             } else {
-              if (fields.length > 1) {
-                queryPart = `${field}:(${esQueryString})`;
-              } else {
-                queryPart = `${field}:"${esQueryString}"`;
-              }
+              queryPart = `${field}:(${esQueryString})`;
             }
           }
         });
